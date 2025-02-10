2c3143cea89a070201212e86203bad1b43751905
Saichand A <saichand@multicorewareinc.com>
2025-02-10 11:16:26 +0000

    #0: Changed shard spec of input tensor to conv manually to increase core count

--- models/experimental/functional_alexnet/tt/ttnn_alexnet.py
+++ models/experimental/functional_alexnet/tt/ttnn_alexnet.py
@@ -6,7 +6,20 @@ import ttnn
 
 
 def Conv(
-    device, x, parameters, path, inp_h, inp_w, k, s, p, width_shard=False, output_layout=ttnn.TILE_LAYOUT, batch_size=1
+    device,
+    x,
+    parameters,
+    path,
+    inp_h,
+    inp_w,
+    k,
+    s,
+    p,
+    width_shard=False,
+    output_layout=ttnn.TILE_LAYOUT,
+    batch_size=1,
+    memory_config=None,
+    change_shard=False,
 ):
     conv_config = ttnn.Conv2dConfig(
         dtype=ttnn.bfloat16,
@@ -33,10 +46,12 @@ def Conv(
 
     if width_shard:
         conv_config.shard_layout = ttnn.TensorMemoryLayout.WIDTH_SHARDED
+    if change_shard:
+        conv_config.shard_layout = None
 
     conv_weight, conv_bias = parameters[path]
 
-    [x, [out_height, out_width]] = ttnn.conv2d(
+    [x, [out_height, out_width], [out_weight, out_bias]] = ttnn.conv2d(
         input_tensor=x,
         weight_tensor=conv_weight,
         in_channels=conv_weight.shape[1],
@@ -54,8 +69,8 @@ def Conv(
         conv_op_cache={},
         debug=False,
         groups=1,
-        memory_config=None,
-        return_weights_and_bias=False,
+        memory_config=memory_config,
+        return_weights_and_bias=True,
         return_output_dim=True,
     )
 
@@ -65,8 +80,27 @@ def Conv(
 def ttnn_alexnet(device, x, parameters):
     batch_size = x.shape[0]
 
+    shard_grid = ttnn.CoreRangeSet(
+        {
+            ttnn.CoreRange(ttnn.CoreCoord(0, 0), ttnn.CoreCoord(7, 7)),
+        }
+    )
+
+    shard_spec = ttnn.ShardSpec(shard_grid, (64, 64), ttnn.ShardOrientation.ROW_MAJOR)
+    sharded_mem_config = ttnn.MemoryConfig(ttnn.TensorMemoryLayout.HEIGHT_SHARDED, ttnn.BufferType.L1, shard_spec)
+
     x, out_height, out_width = Conv(
-        device, x, parameters, "features.0", inp_h=x.shape[1], inp_w=x.shape[2], k=11, s=4, p=2, batch_size=batch_size
+        device,
+        x,
+        parameters,
+        "features.0",
+        inp_h=224,
+        inp_w=224,
+        k=11,
+        s=4,
+        p=2,
+        batch_size=batch_size,
+        memory_config=sharded_mem_config,
     )
 
     x = ttnn.max_pool2d(
@@ -81,8 +115,21 @@ def ttnn_alexnet(device, x, parameters):
         dilation=[1, 1],
     )
 
+    shard_spec = ttnn.ShardSpec(shard_grid, (32, 192), ttnn.ShardOrientation.ROW_MAJOR)
+    sharded_mem_config = ttnn.MemoryConfig(ttnn.TensorMemoryLayout.HEIGHT_SHARDED, ttnn.BufferType.L1, shard_spec)
+
     x, out_height, out_width = Conv(
-        device, x, parameters, "features.3", inp_h=27, inp_w=27, k=5, s=1, p=2, batch_size=batch_size
+        device,
+        x,
+        parameters,
+        "features.3",
+        inp_h=27,
+        inp_w=27,
+        k=5,
+        s=1,
+        p=2,
+        batch_size=batch_size,
+        memory_config=sharded_mem_config,
     )
 
     x = ttnn.max_pool2d(
@@ -97,9 +144,26 @@ def ttnn_alexnet(device, x, parameters):
         dilation=[1, 1],
     )
 
+    shard_spec = ttnn.ShardSpec(shard_grid, (192, 32), ttnn.ShardOrientation.ROW_MAJOR)
+    sharded_mem_config = ttnn.MemoryConfig(ttnn.TensorMemoryLayout.WIDTH_SHARDED, ttnn.BufferType.L1, shard_spec)
+
     x, out_height, out_width = Conv(
-        device, x, parameters, "features.6", inp_h=13, inp_w=13, k=3, s=1, p=1, width_shard=True, batch_size=batch_size
+        device,
+        x,
+        parameters,
+        "features.6",
+        inp_h=13,
+        inp_w=13,
+        k=3,
+        s=1,
+        p=1,
+        width_shard=True,
+        batch_size=batch_size,
+        memory_config=sharded_mem_config,
     )
+
+    shard_spec = ttnn.ShardSpec(shard_grid, (192, 32), ttnn.ShardOrientation.ROW_MAJOR)
+    sharded_mem_config = ttnn.MemoryConfig(ttnn.TensorMemoryLayout.WIDTH_SHARDED, ttnn.BufferType.L1, shard_spec)
 
     x, out_height, out_width = Conv(
         device,
@@ -113,6 +177,7 @@ def ttnn_alexnet(device, x, parameters):
         p=1,
         width_shard=True,
         batch_size=batch_size,
+        memory_config=sharded_mem_config,
     )
 
     x, out_height, out_width = Conv(
@@ -151,12 +216,21 @@ def ttnn_alexnet(device, x, parameters):
 
     x = ttnn.reshape(x, (batch_size, -1), memory_config=ttnn.L1_MEMORY_CONFIG)
 
+    compute_config = ttnn.init_device_compute_kernel_config(
+        device.arch(),
+        math_fidelity=ttnn.MathFidelity.LoFi,
+        math_approx_mode=False,
+        fp32_dest_acc_en=False,
+        packer_l1_acc=False,
+    )
+
     x = ttnn.linear(
         x,
         parameters["classifier.1"][0],
         bias=parameters["classifier.1"][1],
         activation="relu",
         memory_config=ttnn.L1_MEMORY_CONFIG,
+        compute_kernel_config=compute_config,
     )
 
     x = ttnn.linear(
@@ -165,9 +239,14 @@ def ttnn_alexnet(device, x, parameters):
         bias=parameters["classifier.4"][1],
         activation="relu",
         memory_config=ttnn.L1_MEMORY_CONFIG,
+        compute_kernel_config=compute_config,
     )
     x = ttnn.linear(
-        x, parameters["classifier.6"][0], bias=parameters["classifier.6"][1], memory_config=ttnn.L1_MEMORY_CONFIG
+        x,
+        parameters["classifier.6"][0],
+        bias=parameters["classifier.6"][1],
+        memory_config=ttnn.L1_MEMORY_CONFIG,
+        compute_kernel_config=compute_config,
     )
 
     return x
