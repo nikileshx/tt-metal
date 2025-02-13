2c46d1ba8982174f806171f46c9e62a9ba70eab4
Saichand A <saichand@multicorewareinc.com>
2025-02-13 11:22:42 +0000

    #0: Sharded input tensor to pipeline and used sharded last maxpool output

--- models/experimental/functional_alexnet/demo/demo.py
+++ models/experimental/functional_alexnet/demo/demo.py
@@ -26,7 +26,7 @@ def get_dataset(batch_size):
 
     folder_path = "models/experimental/functional_alexnet/demo/images"
 
-    batch_size = min(batch_size, 4)
+    batch_size = min(batch_size, 12)
 
     image_files = [
         os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith((".png", ".jpg", ".jpeg"))
@@ -57,12 +57,29 @@ def run_alexnet_on_imageFolder(device, batch_size):
     parameters = custom_preprocessor(device, state_dict=state_dict)
 
     test_input = get_dataset(batch_size=batch_size)
-    ttnn_input = test_input.permute((0, 2, 3, 1))
 
-    ttnn_input = ttnn.from_torch(ttnn_input, dtype=ttnn.bfloat16, layout=ttnn.ROW_MAJOR_LAYOUT, device=device)
+    core_grid = ttnn.CoreGrid(y=8, x=8)
+    n, c, h, w = test_input.shape
+
+    # sharded mem config for fold input
+    num_cores = core_grid.x * core_grid.y
+    shard_h = (n * w * h + num_cores - 1) // num_cores
+    grid_size = core_grid
+    grid_coord = ttnn.CoreCoord(grid_size.x - 1, grid_size.y - 1)
+    shard_grid = ttnn.CoreRangeSet({ttnn.CoreRange(ttnn.CoreCoord(0, 0), grid_coord)})
+    shard_spec = ttnn.ShardSpec(shard_grid, (shard_h, 16), ttnn.ShardOrientation.ROW_MAJOR)
+    input_mem_config = ttnn.MemoryConfig(
+        ttnn.types.TensorMemoryLayout.HEIGHT_SHARDED, ttnn.types.BufferType.L1, shard_spec
+    )
+    ttnn_input = test_input.permute(0, 2, 3, 1)
+    ttnn_input = ttnn_input.reshape(1, 1, h * w * n, c)
+    ttnn_input = ttnn.from_torch(ttnn_input, dtype=ttnn.bfloat16, layout=ttnn.ROW_MAJOR_LAYOUT)
+    ttnn_input = ttnn.pad(ttnn_input, [1, 1, n * h * w, 16], [0, 0, 0, 0], 0)
+
+    ttnn_input = ttnn_input.to(device, input_mem_config)
 
     with torch.inference_mode():
-        tt_model = TT_Alexnet(device, ttnn_input.shape, parameters)
+        tt_model = TT_Alexnet(device, test_input.shape, parameters)
         ttnn_output_tensor = tt_model(ttnn_input)
         ttnn_output_tensor = ttnn.to_torch(ttnn_output_tensor)
         ttnn_predicted_probabilities = torch.nn.functional.softmax(ttnn_output_tensor, dim=1)
@@ -89,7 +106,7 @@ def run_alexnet_on_imageFolder(device, batch_size):
 
 
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
-@pytest.mark.parametrize("batch_size", [1, 2, 4, 8, 12])
+@pytest.mark.parametrize("batch_size", [1, 2, 8, 12])
 def test_alexnet_on_imageFolder(device, batch_size):
     disable_persistent_kernel_cache()
     disable_compilation_reports()
